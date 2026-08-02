21b2ab209d8256011b8577d1230497b749926f88
kerildash <kerildash@gmail.com>
2026-08-02 20:40:38 +0300

    add project

---
+++ PlatformService/PlatformService/Data/PlatformDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace PlatformService.Data;
+
+public class PlatformDbContext
+{
+    
+}
---
+++ PlatformService/PlatformService/Models/Platform.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models;
+
+public class Platform
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Publisher { get; set; }
+}
