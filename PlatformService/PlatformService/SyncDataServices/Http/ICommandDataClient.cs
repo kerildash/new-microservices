e9a159690c85631a874174be22189874e74ed16e
kerildash <kerildash@gmail.com>
2026-08-07 02:16:53 +0300

    Add and register in a DI container an http client factory

---
+++ PlatformService/PlatformService/SyncDataServices/Http/ICommandDataClient.cs
@@ -0,0 +1,8 @@
+﻿using PlatformService.Dto;
+
+namespace PlatformService.SyncDataServices.Http;
+
+public interface ICommandDataClient
+{
+    Task SendPlatformToCommandService(PlatformResponseDto platform);
+}
