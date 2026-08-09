f542b15018ed341d2e23ecbcbf29c1debcf82279
kerildash <kerildash@gmail.com>
2026-08-09 06:24:37 +0300

    Add connection string building from environment variables and preparing the db with helper.

--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -1,6 +1,6 @@
-using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.Data.Helpers;
 using PlatformService.Models;
 using PlatformService.SyncDataServices.Http;
 
@@ -13,25 +13,46 @@ builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<PlatformDbContext>(options => options.UseInMemoryDatabase("InMemory"));
+builder.Services.AddDbContext<PlatformDbContext>(options => options.UseNpgsql(BuildConnectionString()));
 builder.Services.AddScoped<IRepository<Platform>, PlatformRepository>();
+builder.Services.AddScoped<DatabasePreparationHelper>();
 
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
 var app = builder.Build();
 
+await PrepareDb(app);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    Console.WriteLine("dev environment");
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
-    Prep.Populate(app);
 }
 
 app.MapControllers();
-
 app.UseHttpsRedirection();
 
 app.Run();
+return;
+
+static string BuildConnectionString()
+{
+    var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+    var databaseName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+    var userName = Environment.GetEnvironmentVariable("POSTGRES_USER");
+    var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+    var connectionString = $"Host={host};Port=5432;Database={databaseName};Username={userName};Password={password}";
+    Console.WriteLine($"Connection string: {connectionString}");
+    return connectionString;
+}
+
+static async Task PrepareDb(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var dbPreparation = scope.ServiceProvider.GetRequiredService<DatabasePreparationHelper>();
+    await dbPreparation.Migrate();
+    await dbPreparation.Populate();
+}
