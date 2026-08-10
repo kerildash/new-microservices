abad908df6702fe55256f3c73c0b624d90564c4a
kerildash <kerildash@gmail.com>
2026-08-11 00:07:25 +0300

    CommandService: add logging with Serilog and Seq

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 namespace CommandService;
 
 public static class Program
@@ -5,6 +7,9 @@ public static class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration));
 
         // Add services to the container.
         builder.Services.AddControllers();
