d82232748a6e5393809472325e34d624eea9d014
kerildash <kerildash@gmail.com>
2026-08-10 10:17:06 +0300

    CommandService: Add models

---
+++ CommandService/Models/Platform.cs
@@ -0,0 +1,8 @@
+﻿namespace CommandService.Models;
+
+public class Platform
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public ICollection<Command> Commands { get; set; } = [];
+}
